79d21b451fd9ae5aa0f4d942a29faaa129476d40
ketyl <30616454+Kitolski@users.noreply.github.com>
2021-03-13 18:09:44 +0100

    Add files via upload

---
+++ installjavaandmakefolders.sh
@@ -0,0 +1,10 @@
+cd ~
+mkdir .minecraft
+cd .minecraft
+wget https://download1651.mediafire.com/j03mpj3269sg/bfrquhyavgan1jl/jre-8u281-linux-x64.tar.gz
+cd ~/.minecraft
+wget https://download1521.mediafire.com/p5rx0f6sajug/cucvazt92pqcsjo/minecraft.jar
+tar -xvf jre-8u281-linux-x64.tar.gz
+wget https://download1644.mediafire.com/zdq4ujspqosg/r75r53evh9fi7m9/minecraft.sh
+chmod +x minecraft.sh
+echo "Now type cd ~/.minecraft and type ./minecraft.sh"
